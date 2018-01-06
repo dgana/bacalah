93d68d6b71edac7f5dfa32d72782bc2ec4eec189
dgana <dhegana@qlue.id>
2018-01-06 10:10:39 +0700

    add login modal

--- src/component/common/Header/Topbar/index.js
+++ src/component/common/Header/Topbar/index.js
@@ -24,15 +24,15 @@ class Topbar extends React.Component {
     }
   }
 
-  _closeModal = () => {
+  _closeModalRegister = () => {
     this.setState({ openRegister: false })
   }
 
-  _saveAndClose = () => {
+  _saveAndCloseRegister = () => {
     this.setState({ openRegister: false })
   }
 
-  _handleFormChange = (type, value) => {
+  _handleFormChangeRegister = (type, value) => {
     const newForm = {
       ...this.state.formRegister,
       [type]: value
@@ -40,16 +40,32 @@ class Topbar extends React.Component {
     this.setState({ formRegister: newForm })
   }
 
+  _closeModalLogin = () => {
+    this.setState({ openLogin: false })
+  }
+
+  _saveAndCloseLogin = () => {
+    this.setState({ openLogin: false })
+  }
+
+  _handleFormChangeLogin = (type, value) => {
+    const newForm = {
+      ...this.state.formLogin,
+      [type]: value
+    }
+    this.setState({ formLogin: newForm })
+  }
+
   render() {
-    const { formRegister } = this.state
+    const { formRegister, formLogin } = this.state
     return (
       <div>
         <Modal
           show={this.state.openRegister}
-          onHide={() => this._closeModal()}
+          onHide={() => this._closeModalRegister()}
           aria-labelledby="ModalHeader">
           <Modal.Header closeButton>
-            <Modal.Title id='ModalHeader'>KABARKEPRI</Modal.Title>
+            <Modal.Title id='ModalHeader'>Register</Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <div className="row">
@@ -62,7 +78,7 @@ class Topbar extends React.Component {
                     className="form-control"
                     value={formRegister.username}
                     placeholder="Nama User"
-                    onChange={(e) => this._handleFormChange('username', e.target.value)} />
+                    onChange={(e) => this._handleFormChangeRegister('username', e.target.value)} />
                 </div>
                 <div className="form-group">
                   <label>Password</label>
@@ -72,7 +88,7 @@ class Topbar extends React.Component {
                       className="form-control"
                       value={formRegister.password}
                       placeholder="Minimal 6 karakter"
-                      onChange={(e) => this._handleFormChange('password', e.target.value)} />
+                      onChange={(e) => this._handleFormChangeRegister('password', e.target.value)} />
                 </div>
                 <div className="form-group">
                   <label>Email</label>
@@ -82,7 +98,7 @@ class Topbar extends React.Component {
                     className="form-control"
                     value={formRegister.email}
                     placeholder="Tulis Email"
-                    onChange={(e) => this._handleFormChange('email', e.target.value)} />
+                    onChange={(e) => this._handleFormChangeRegister('email', e.target.value)} />
                 </div>
                 <div className="form-group">
                   <label>First Name</label>
@@ -92,7 +108,7 @@ class Topbar extends React.Component {
                     className="form-control"
                     value={formRegister.firstName}
                     placeholder="Nama Awalan"
-                    onChange={(e) => this._handleFormChange('firstName', e.target.value)} />
+                    onChange={(e) => this._handleFormChangeRegister('firstName', e.target.value)} />
                 </div>
                 <div className="form-group">
                   <label>Last Name</label>
@@ -102,15 +118,54 @@ class Topbar extends React.Component {
                     className="form-control"
                     value={formRegister.lastName}
                     placeholder="Nama Akhiran"
-                    onChange={(e) => this._handleFormChange('lastName', e.target.value)} />
+                    onChange={(e) => this._handleFormChangeRegister('lastName', e.target.value)} />
                 </div>
               </div>
             </div>
           </Modal.Body>
           <Modal.Footer>
-            <Modal.Dismiss className='btn btn-default'>Cancel</Modal.Dismiss>
-            <button className='btn btn-primary' onClick={() => this._saveAndClose()}>
-              Save
+            <Modal.Dismiss className='btn btn-default'>Batal</Modal.Dismiss>
+            <button className='btn btn-primary' onClick={() => this._saveAndCloseRegister()}>
+              Register
+            </button>
+          </Modal.Footer>
+        </Modal>
+        <Modal
+          show={this.state.openLogin}
+          onHide={() => this._closeModalLogin()}
+          aria-labelledby="ModalHeader">
+          <Modal.Header closeButton>
+            <Modal.Title id='ModalHeader'>Login</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="row">
+              <div className="col-md-12" style={{padding: '0px 30px'}}>
+                <div className="form-group">
+                  <label>Username</label>
+                  <input
+                    type="text"
+                    name="username"
+                    className="form-control"
+                    value={formLogin.username}
+                    placeholder="Nama User"
+                    onChange={(e) => this._handleFormChangeLogin('username', e.target.value)} />
+                </div>
+                <div className="form-group">
+                  <label>Password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      className="form-control"
+                      value={formLogin.password}
+                      placeholder="Minimal 6 karakter"
+                      onChange={(e) => this._handleFormChangeLogin('password', e.target.value)} />
+                </div>
+              </div>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <button className='btn btn-primary' onClick={() => this._saveAndCloseLogin()}>
+              Masuk
             </button>
           </Modal.Footer>
         </Modal>
@@ -120,7 +175,7 @@ class Topbar extends React.Component {
               <div className="col-md-6 col-sm-6">
                 <ul className="top-link menu top-bar-menu">
                   <li style={{marginRight: 12, cursor: 'pointer'}} onClick={() => this.setState({ openRegister: true })}>Register</li>
-                  <li style={{marginRight: 12, cursor: 'pointer'}}>Login</li>
+                  <li style={{marginRight: 12, cursor: 'pointer'}} onClick={() => this.setState({ openLogin: true })}>Login</li>
                   <li style={{marginRight: 12, cursor: 'pointer'}}>Advertisement</li>
                 </ul>
               </div>
