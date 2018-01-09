fb2c49d4c6a15904d1cc84fc9356b5f4526684d0
dgana <dhegana@qlue.id>
2018-01-09 09:38:20 +0700

    push build file

--- src/component/common/Footer/Bottombar/index.js
+++ src/component/common/Footer/Bottombar/index.js
@@ -1,18 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => (
   <footer className="site-footer">
     <div className="container footer-view">
       <div className="row">
         <div className="col-md-6 col-sm-6 copyright">
-          <span>COPYRIGHT @PT Media Gemah Ripah </span>
+          <span>COPYRIGHT @ PT Media Gemah Ripah </span>
         </div>
-        {/* <div className="col-md-6 col-sm-6 footer-link">
+        <div className="col-md-6 col-sm-6 footer-link">
           <ul className="menu">
-            <li><a href="">TERMS OF US</a></li>
-            <li><a href="">PRIVACY POLICY</a></li>
+            <li><Link to={"/"}>TENTANG KAMI</Link></li>
+            <li>PEDOMAN MEDIA SIBER</li>
           </ul>
-        </div> */}
+        </div>
       </div>
     </div>
   </footer>
